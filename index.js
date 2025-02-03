f40be67d7317bf4d6a11dc1f93ed14eba23db7ca
Tapasvi Chhabhaiya <117344055+TTHPATEL@users.noreply.github.com>
2025-02-03 13:39:49 +0530

    REDO

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ let scheduleMail = [
   {
     scheduleMailID: 1,
     template: "Salary Increment Letter",
-    schedule: "2027-02-14T13:25:00Z",
+    schedule: "2026-02-02T04:14",
     recipientGroupName: "Marketing Team",
     recipient: ["thchhabhaiya@gmail.com"],
     status: "Pending",
